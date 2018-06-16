124f154e821c486a009768dce27acd9ad0f87e3a
frankie-boy <frankieboylan@hotmail.co.uk>
2018-06-16 10:48:06 +0100

    Removed unused String

--- app/src/main/java/com/novoda/frankboylan/ncu/MainActivity.java
+++ app/src/main/java/com/novoda/frankboylan/ncu/MainActivity.java
@@ -7,9 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.novoda.frankboylan.ncu.nodes.NodeMap;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String JSON_STRING_TEMP = "{ \"metadata\": { \"title\":\"engineering\" }, \"layers\": [{ \"layer-number\":\"0\", \"nodes\": [ {\"node-id\":\"1\", \"node-status\":\"UNLOCKED\", \"pos-x\":\"0\", \"parent\":[{\"node-id\":\"0\"}], \"children\":[{\"node-id\":\"4\"}, {\"node-id\":\"5\"}]}, {\"node-id\":\"2\", \"node-status\":\"UNLOCKED\", \"pos-x\":\"2\", \"parent\":[{\"node-id\":\"0\"}], \"children\":[{\"node-id\":\"5\"}]}, {\"node-id\":\"3\", \"node-status\":\"UNLOCKED\", \"pos-x\":\"4\", \"parent\":[{\"node-id\":\"0\"}], \"children\":[{\"node-id\":\"6\"}]} ]\t},{ \"layer-number\":\"1\", \"nodes\": [ {\"node-id\":\"4\", \"node-status\":\"LOCKED\", \"pos-x\":\"0\", \"parent\":[{\"node-id\":\"1\"}], \"children\":[{\"node-id\":\"7\"}]}, {\"node-id\":\"5\", \"node-status\":\"LOCKED\", \"pos-x\":\"2\", \"parent\":[{\"node-id\":\"1\"}, {\"node-id\":\"2\"}], \"children\":[{\"node-id\":\"8\"}]}, {\"node-id\":\"6\", \"node-status\":\"LOCKED\", \"pos-x\":\"4\", \"parent\":[{\"node-id\":\"3\"}], \"children\":[]} ]\t},{ \"layer-number\":\"2\", \"nodes\": [ {\"node-id\":\"7\", \"node-status\":\"LOCKED\", \"pos-x\":\"1\", \"parent\":[{\"node-id\":\"4\"}], \"children\":[{\"node-id\":\"9\"}, {\"node-id\":\"10\"}]}, {\"node-id\":\"8\", \"node-status\":\"LOCKED\", \"pos-x\":\"3\", \"parent\":[{\"node-id\":\"5\"}], \"children\":[{\"node-id\":\"10\"}, {\"node-id\":\"11\"}]} ] },{ \"layer-number\":\"3\", \"nodes\": [ {\"node-id\":\"9\", \"node-status\":\"LOCKED\", \"pos-x\":\"1\", \"parent\":[{\"node-id\":\"7\"}], \"children\":[]}, {\"node-id\":\"10\", \"node-status\":\"LOCKED\", \"pos-x\":\"3\", \"parent\":[{\"node-id\":\"7\"}, {\"node-id\":\"8\"}], \"children\":[]}, {\"node-id\":\"11\", \"node-status\":\"LOCKED\", \"pos-x\":\"4\", \"parent\":[{\"node-id\":\"8\"}], \"children\":[]} ]\t} ] }\n";
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
